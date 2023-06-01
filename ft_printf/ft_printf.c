a4c8511d2936ed2a50de38e0cef5c0eeb0e39718
TCostad <tadeu.costa47@gmail.com>
2023-06-01 14:36:51 +0100

    Começando codificação do printf

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: tcosta-d < tcosta-d@student.42porto.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 14:35:14 by tcosta-d          #+#    #+#             */
-/*   Updated: 2023/05/19 14:52:51 by tcosta-d         ###   ########.fr       */
+/*   Updated: 2023/05/30 14:59:49 by tcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,18 @@
 
 int	ft_printf(const char *format, ...)
 {
-	
+	va_list args;
+	int		done;
+
+	va_start(args, format);
+	// O que eu realmente quero dizer fazendo o que fiz até aqui --> ft_printf_core(format, args);
+
+	while (*format)
+	{
+		
+	}
+
+	va_end(args);
+
+	return (done);
 }
