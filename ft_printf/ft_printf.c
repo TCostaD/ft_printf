14415275f2567f929f2bdd9d9effacf88f7e52aa
TCostad <tadeu.costa47@gmail.com>
2023-06-01 16:51:35 +0100

    adicionando struct no projeto

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,13 +6,27 @@
 /*   By: tcosta-d < tcosta-d@student.42porto.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 14:35:14 by tcosta-d          #+#    #+#             */
-/*   Updated: 2023/06/01 15:00:50 by tcosta-d         ###   ########.fr       */
+/*   Updated: 2023/06/01 16:47:45 by tcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 #include <stdio.h>
 
+t_print	ft_init_print(t_print *print)
+{
+	print->width = 0;
+	print->precision = 0;
+	print->zero = 0;
+	print->point = 0;
+	print->dash = 0;
+	print->len = 0;
+	print->sign = 0;
+	print->is_zero = 0;
+	print->percent = 0;
+	print->space = 0;
+	return (print);
+}
 
 int	ft_printf(const char *format, ...)
 {
--- ft_printf/libftprintf.h
+++ ft_printf/libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: tcosta-d < tcosta-d@student.42porto.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 14:35:06 by tcosta-d          #+#    #+#             */
-/*   Updated: 2023/06/01 15:21:35 by tcosta-d         ###   ########.fr       */
+/*   Updated: 2023/06/01 15:31:14 by tcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # include <unistd.h>
 # include <stdarg.h>
 
-typedef struct	s_print
+typedef struct s_print
 {
 	va_list	args;
 	int		width;
