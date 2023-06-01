7d80c5e8020c9e8753b8d3ba12ffe0a603367a74
TCostad <tadeu.costa47@gmail.com>
2023-06-01 15:27:27 +0100

    Mexendo no ft_printf e na lib

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,16 +6,18 @@
 /*   By: tcosta-d < tcosta-d@student.42porto.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 14:35:14 by tcosta-d          #+#    #+#             */
-/*   Updated: 2023/05/30 14:59:49 by tcosta-d         ###   ########.fr       */
+/*   Updated: 2023/06/01 15:00:50 by tcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
+#include <stdio.h>
+
 
 int	ft_printf(const char *format, ...)
 {
 	va_list args;
-	int		done;
+	int		len;
 
 	va_start(args, format);
 	// O que eu realmente quero dizer fazendo o que fiz até aqui --> ft_printf_core(format, args);
@@ -27,5 +29,5 @@ int	ft_printf(const char *format, ...)
 
 	va_end(args);
 
-	return (done);
+	return (len);
 }
--- ft_printf/libftprintf.h
+++ ft_printf/libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: tcosta-d < tcosta-d@student.42porto.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 14:35:06 by tcosta-d          #+#    #+#             */
-/*   Updated: 2023/05/19 14:35:35 by tcosta-d         ###   ########.fr       */
+/*   Updated: 2023/06/01 15:21:35 by tcosta-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,21 @@
 
 # include <unistd.h>
 # include <stdarg.h>
+
+typedef struct	s_print
+{
+	va_list	args;
+	int		width;
+	int		precision;
+	int		zero;
+	int		point;
+	int		dash;
+	int		len;
+	int		sign;
+	int		is_zero;
+	int		percent;
+	int		space;
+}	t_print;
 
 int		ft_printf(const char *format, ...);
 
