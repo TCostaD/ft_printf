1f120282ba56574833bb6fa3c8f6ab2f1d816ecc
oroitzzabala <tadeu.costa47@gmail.com>
2023-06-11 05:43:16 +0100

    Consertando caso em que tento printar ponteiro nulo

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -16,22 +16,26 @@
 int	main(void)
 {
 	void *ptr = (void *)0x7ffeefbff5c8;
+	char *str = NULL;
 
 	printf("função original\n");
 	ft_printf("minha função\n");
 	printf("\n");
 
+	printf("o: String nula: %s\n", str);
+	ft_printf("m: String nula: %s\n\n", str);
+
 	printf("o: número hexadecimal minúsculo -> %x\n", 42);
-	ft_printf("m: número hexadecimal minúsculo -> %x\n", 42);
+	ft_printf("m: número hexadecimal minúsculo -> %x\n\n", 42);
 
 	printf("o: número hexadecimal maiúsculo -> %X\n", -42);
-	ft_printf("m: número hexadecimal maiúsculo -> %X\n", -42);
+	ft_printf("m: número hexadecimal maiúsculo -> %X\n\n", -42);
 
 	printf("o: número decimal -> %d\n", 42);
-	ft_printf("m: número decimal -> %d\n", 42);
+	ft_printf("m: número decimal -> %d\n\n", 42);
 
 	printf("o: número decimal sem sinal: %u\n", -5);
-	ft_printf("m: número decimal sem sinal: %u\n", -5);
+	ft_printf("m: número decimal sem sinal: %u\n\n", -5);
 
 	printf("o: Endereço do ponteiro: %p\n", ptr);
 	ft_printf("m: Endereço do ponteiro: %p\n", ptr);
--- ft_printf/src/ft_putchars.c
+++ ft_printf/src/ft_putchars.c
@@ -23,7 +23,7 @@ int	ft_putstr(char *s)
 
 	count = 0;
 	if (!s)
-		return (0);
+		return (write(1, "(null)", 6));
 	while (*s)
 		count += ft_putchar(*s++);
 	return (count);
